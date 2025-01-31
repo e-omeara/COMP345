a54e58c8309f12408519af1e60dae1f628657c5b
elizabethomeara <elizabeth.omeara@mail.mcgill.ca>
2025-01-31 11:03:29 -0500

    added cpp and h files for a1q1

---
+++ Map/Map.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+#include <cmath>
+#include "Map.h" 
+#include "MapMaker.h" 
+using namespace std;
+
+//attributes
+//length
+//width
+//map 2d array of chars
+//array of struct path with atributes int x int y
+//critter 1d array of Critter objects
+
+
+//map constructor with size
+//
+
+
+//MAIN FUNCS
+
+//display()
+//
+//return string object
+
+
+
+//HELPER FUNCS
+
+
+
+
+
---
+++ Map/MapMaker.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//receive IO
+
+//Construct the map class
+//Map map = new Map()
+
+//display the map
+//map.display()
+
+
+//add entrance
+//map.setEntrance(x,y)
+
+//add paths OR backspace OR createExit or QuitEditor
+
+//playGame
+//clear and show "you're playing the game now"
+
+//backToMain
+//clear and show "youre in the main menu" place holder for mm
+
+
+
+
+
+
+
