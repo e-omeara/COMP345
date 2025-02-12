a71abd3d1ed1f2a40a42cdd03f7be65c2052f2a3
sang-hh <sanghyun.hong438@gmail.com>
2025-02-12 14:43:58 -0500

    Update critter.cpp with test case

--- critter/critter.cpp
+++ critter/critter.cpp
@@ -30,6 +30,10 @@ bool Critter::hasReachedEnd() const {
     return positionIndex == path.size() - 1;
 }
 
+//Get HP of Critter
+int Critter::getHP() const { 
+    return hp; 
+}
 
 //Get strength value
 int Critter::getStrength() const {
@@ -46,6 +50,11 @@ Position Critter::getPosition() const {
     return path[positionIndex];
 }
 
+//Get Critter Type
+std::string Critter::getType() const { 
+    return "Basic Critter"; 
+}
+
 //Move the critter along the path
 void Critter::move() {
     if (positionIndex + 1 < path.size()) {
@@ -59,6 +68,37 @@ void Critter::move() {
     }
 }
 
+//FastCritter attributes(Moves Twice as Fast)
+FastCritter::FastCritter(std::vector<Position> path) 
+    : Critter(20, 1, 3, 1, 7, path) {}
+
+void FastCritter::move() {
+    positionIndex += (speed * 2);
+    if (positionIndex >= path.size()) {
+        positionIndex = path.size() - 1;
+    }
+}
+
+std::string FastCritter::getType() const { 
+    return "Fast Critter"; 
+}
+
+//TankCritter attributes (More HP but Moves Slower)
+TankCritter::TankCritter(std::vector<Position> path)
+    : Critter(50, 1, 5, 1, 10, path) {}
+
+std::string TankCritter::getType() const { 
+    return "Tank Critter"; 
+}
+
+//BossCritter (High HP and High Strength)
+BossCritter::BossCritter(std::vector<Position> path)
+    : Critter(100, 1, 10, 1, 20, path) {}
+
+std::string BossCritter::getType() const { 
+    return "Boss Critter"; 
+}
+
 //CritterGroup constructor
 CritterGroup::CritterGroup(int waveLevel, std::vector<Position> path) {
     generateWave(waveLevel, path);
@@ -66,14 +106,10 @@ CritterGroup::CritterGroup(int waveLevel, std::vector<Position> path) {
 
 //Generate a wave of critters
 void CritterGroup::generateWave(int waveLevel, std::vector<Position> path) {
-    int baseHP = 10 + waveLevel * 5;
-    int baseSpeed = 1 + waveLevel / 2;
-    int baseStrength = 2 + waveLevel;
-    int baseReward = 5 + waveLevel * 2;
-    
-    for (int i = 0; i < 5 + waveLevel; ++i) {
-        critters.push_back(Critter(baseHP, baseSpeed, baseStrength, waveLevel, baseReward, path));
-    }
+    for (int i = 0; i < 3; ++i) critters.emplace_back(FastCritter(path));
+    for (int i = 0; i < 3; ++i) critters.emplace_back(TankCritter(path));
+    //Only one boss per wave
+    critters.emplace_back(BossCritter(path));  
 }
 
 //Move all critters in the group
@@ -94,4 +130,32 @@ void CritterGroup::moveCritters(int& playerCoins) {
             std::cout << "Critter killed! Player earned " << critter.getReward() << " coins!\n";
         }
     }
+}
+
+//Test Case for Critters
+int main() {
+    std::vector<Position> path = { {0,0}, {1,0}, {2,0}, {3,0}, {4,0}, {5,0}, {6,0}, {7,0} };
+
+    //Create Different Critter Types
+    FastCritter fc(path);
+    TankCritter tc(path);
+    BossCritter bc(path);
+
+    //Store them in a vector for testing
+    std::vector<Critter*> critters = { &fc, &tc, &bc };
+
+    //Testing five turns
+    for (int turn = 0; turn < 5; ++turn) {
+        std::cout << "\nTurn " << turn + 1 << ":\n";
+
+        for (auto critter : critters) {
+            critter->move();
+            Position pos = critter->getPosition();
+            std::cout << critter->getType() << " at (" << pos.x << ", " << pos.y << ") | HP: "
+                      << critter->getHP() << " | Speed: " << critter->getSpeed() << " | Strength: "
+                      << critter->getStrength() << "\n";
+        }
+    }
+
+    return 0;
 }
